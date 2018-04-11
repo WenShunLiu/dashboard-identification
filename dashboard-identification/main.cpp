2a902b202b209f9e2aa6c73d5ed1618a2c3f5a94
liuwenshun <liuwenshun@u51.com>
2018-04-11 20:05:41 +0800

    Initial Commit

---
+++ dashboard-identification/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  dashboard-identification
+//
+//  Created by LWS on 2018/4/11.
+//  Copyright © 2018年 LWS. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
